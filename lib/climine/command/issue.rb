bd4718d184121b606cb83c6833d9ed4e4b77cfec
yagince <straitwalk@gmail.com>
2013-11-06 23:53:43 +0900

    add seach option for issue

--- lib/climine/command/issue.rb
+++ lib/climine/command/issue.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 module Climine::Command
   module Issue
     def self.included base
@@ -6,7 +7,8 @@ module Climine::Command
         desc "issue [TICKET_NO]", "get Redmine Issues."
         option :limit, type: :numeric, aliases: '-l', banner: "LIMIT", desc: "limit of search result (default: 25)", deafult: 25
         option :offset, type: :numeric, aliases: '-o', banner: "OFFSET", desc: "page of seach result(0 origin) (default: 0)"
-        option :project_id, type: :numeric, aliases: '-p', banner: "PROJECT_ID", desc: "id of RedmineProject (default: all project)"
+        option :project_id, type: :numeric, aliases: '-p', banner: "PROJECT_ID", desc: "id of RedmineProject ( Please search by `climine project` ) (default: all project)"
+        option :assigned_to_id, type: :numeric, aliases: '-u', banner: "USER_ID", desc: "ID of the user being assigned ( Please search by `climine user` )"
         def issue(id=nil)
           if id
             render :issue, redmine.issue(id)
