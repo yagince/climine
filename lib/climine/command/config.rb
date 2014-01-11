d7806dca7535a4c0962c67072735eef5b5d3aee5
yagince <straitwalk@gmail.com>
2014-01-12 01:54:32 +0900

    add 'editor' to init command options

--- lib/climine/command/config.rb
+++ lib/climine/command/config.rb
@@ -6,6 +6,9 @@ module Climine::Command
     TEMPLATE = <<-ERB
 url: <%= options[:url] %>
 apikey: <%= options[:key] %>
+<% if options[:editor] -%>
+editor: <%= options[:editor] %>
+<% end -%>
     ERB
 
     default_command :init
@@ -13,22 +16,11 @@ apikey: <%= options[:key] %>
     desc "init ", "initialize config.yml"
     option :url, required: true, type: :string, aliases: '-u', banner: "RedmineURL", desc: "Your Redmine's URL"
     option :key, required: true, type: :string, aliases: '-k', banner: "API-Access-Key", desc: "Your API Access Key"
+    option :editor, required: false, type: :string, aliases: '-e', banner: "Editor command or path", desc: "Your favorite editor ex) emacs or vim or /usr/bin/emacs etc..."
     def init
-      open(CONFIG, 'w'){|file| file.write(ERB.new(TEMPLATE).result(binding)) }
+      open(CONFIG, 'w'){|file| file.write(ERB.new(TEMPLATE, nil, "-").result(binding)) }
       puts "create [ #{File.expand_path(CONFIG)} ]"
     end
-    # def self.included base
-    #   base.class_eval do
 
-    #     desc "init ", "initialize config.yml"
-    #     option :url, required: true, type: :string, aliases: '-u', banner: "RedmineURL", desc: "Your Redmine's URL"
-    #     option :key, required: true, type: :string, aliases: '-k', banner: "API-Access-Key", desc: "Your API Access Key"
-    #     def init
-    #       open(CONFIG, 'w'){|file| file.write(ERB.new(TEMPLATE).result(binding)) }
-    #       puts "create [ #{File.expand_path(CONFIG)} ]"
-    #     end
-
-    #   end
-    # end
   end
 end
